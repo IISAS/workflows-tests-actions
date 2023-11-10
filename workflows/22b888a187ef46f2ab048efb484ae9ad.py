093c9a0fc4119be36c609aaca25aa8b686ad4a59
Vinícius Vaz <vinicvaz95@gmail.com>
2023-11-10 08:22:20 -0300

    Create file

---
+++ workflows/22b888a187ef46f2ab048efb484ae9ad.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from dateutil.parser import parse
+from airflow import DAG
+from domino.task import Task
+
+dag_config_0 = {'start_date': '2023-11-10', 'schedule': None, 'catchup': False, 'dag_id': '22b888a187ef46f2ab048efb484ae9ad'}
+
+# Parse datetime values
+dt_keys = ['start_date', 'end_date']
+dag_config = { k: (v if k not in dt_keys else parse(v)) for k, v in dag_config_0.items()}
+
+with DAG(**dag_config) as dag:
+    SimpleLogP_0298c1669d404e08b631ebe1490e1c45 = Task(
+        dag, 
+        task_id='SimpleLogP_0298c1669d404e08b631ebe1490e1c45',
+        workspace_id=1,
+        workflow_shared_storage={'source': 'None', 'mode': 'Read/Write', 'provider_options': {}},
+        container_resources={'requests': {'cpu': '100.0m', 'memory': '128.0Mi'}, 'limits': {'cpu': '100.0m', 'memory': '128.0Mi'}, 'use_gpu': False},
+        piece={'name': 'SimpleLogPiece', 'source_image': 'ghcr.io/tauffer-consulting/default_domino_pieces:0.5.0-group0', 'repository_url': 'https://github.com/Tauffer-Consulting/default_domino_pieces', 'repository_version': '0.5.0'},
+        piece_input_kwargs={'input_str': 'default value', 'input_int': 10, 'input_float': 10.5, 'input_bool': False, 'input_enum': 'option1', 'input_date': '2023-01-01', 'input_time': '16:20:00', 'input_datetime': '2023-01-01T16:20:00', 'input_array': ['default_1', 'default_2', 'default_3'], 'input_code': "print('Hello world!')"}
+    )()
+
